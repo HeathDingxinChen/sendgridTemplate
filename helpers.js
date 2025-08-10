ad64bf1702469be5ec4679ee5a8bc6cfb2115537
Heath CHEN <heath.dingxin.chen@accenture.com>
2025-08-11 01:12:23 +0800

    feat: set up initial project structure and render email template
    - Create project directory and set up file structure
    - Add Node.js and Handlebars dependencies
    - Create email template data and helpers
    - Implement template rendering logic
    - Generate preview HTML file

---
+++ helpers.js
@@ -0,0 +1,17 @@
+module.exports = function (Handlebars) {
+    Handlebars.registerHelper('or', function () {
+      return Array.prototype.some.call(arguments, function (arg) {
+        return !!arg;
+      });
+    });
+    Handlebars.registerHelper('equals', function (a, b, options) {
+        if (a === b) {
+          // 满足条件，渲染块内内容
+          return options.fn(this);
+        }
+        // 不满足条件，渲染 else 块（{{else}} 部分），如果有的话
+        return options.inverse(this);
+      });
+    // 你可以在这里继续注册更多 helpers
+  };
+  
---
+++ render.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const Handlebars = require('handlebars');
+
+// 1. 注册 helpers
+require('./helpers')(Handlebars);
+
+// 2. 读取模板 & 数据
+const templateSource = fs.readFileSync('./template.hbs', 'utf8');
+const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+
+// 3. 编译模板
+const template = Handlebars.compile(templateSource);
+
+// 4. 渲染输出 HTML
+const html = template(data);
+
+// 5. 写入到预览文件
+fs.writeFileSync('./preview.html', html, 'utf8');
+
+console.log('✅ 模板已渲染到 preview.html');
