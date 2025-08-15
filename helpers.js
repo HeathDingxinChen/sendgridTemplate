3f024b930e5863025b565eaa627cf84367025952
ziqiu.jiang <ziqiu.jiang@accenture.com>
2025-08-15 18:11:17 +0800

    align as sendgrid

--- helpers.js
+++ helpers.js
@@ -12,6 +12,11 @@ module.exports = function (Handlebars) {
         // 不满足条件，渲染 else 块（{{else}} 部分），如果有的话
         return options.inverse(this);
       });
+
+    // 新增 add、mod、eq helpers
+    Handlebars.registerHelper('add', (a, b) => a + b);
+    Handlebars.registerHelper('mod', (a, b) => a % b);
+    Handlebars.registerHelper('eq', (a, b) => a === b);
     // 你可以在这里继续注册更多 helpers
   };
   
